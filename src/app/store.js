cc211166d91a431cb36bf73d5f6dbe127a363a5c
arfat132 <arfatbegum90@gmail.com>
2022-09-11 22:46:06 +0300

    create api slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { apiSlice } from '../features/api/apiSlice';
+import authReducer from "../features/auth/authSlice";
+import conversationsReducer from "../features/auth/onversationsSlice";
+import messagesReducer from "../features/auth/messagesSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+    conversations: conversationsReducer,
+    messages: messagesReducer,
   },
+
+  devTools: process.env.NODE_ENV !== "production",
+  middleware: (getDefaultMiddlewares)=> getDefaultMiddlewares().concat(apiSlice.middleware),
 });
